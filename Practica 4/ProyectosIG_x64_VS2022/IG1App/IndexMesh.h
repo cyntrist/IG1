d210b7433876941f96bfd695a1675d6bdbf5ad5a
cyntrist <118978961+cyntrist@users.noreply.github.com>
2024-04-12 16:32:18 +0200

    apartado 62

---
+++ Practica 4/ProyectosIG_x64_VS2022/IG1App/IndexMesh.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Mesh.h"
+
+class IndexMesh : public Mesh
+{
+protected :
+	GLuint* vIndices = nullptr; // tabla de índices
+	GLuint nNumIndices = 0;
+	//...
+
+public:
+	IndexMesh() { mPrimitive = GL_TRIANGLES; }
+	~ IndexMesh() override { delete [] vIndices; }
+	void render() const override;
+	void draw() const override;
+	//...
+	glm::vec3 calculoVectorNormalPorNewell();
+};
