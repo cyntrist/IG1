7f36040366ae4853085fb9c1619589d28c5b950e
ineprimo <ineprimo@ucm.es>
2024-04-12 15:57:05 +0200

    no quiero que se borre....

--- Practica 4/ProyectosIG_x64_VS2022/IG1App/Entity.cpp
+++ Practica 4/ProyectosIG_x64_VS2022/IG1App/Entity.cpp
@@ -661,3 +661,22 @@ void Grass::render(const dmat4& modelViewMat) const
 		mTexture->unbind();
 	}
 }
+
+Sphere::Sphere(GLdouble rr)
+{
+	r = rr;
+}
+
+void Sphere::render(glm::dmat4 const& modelViewMat) const
+{
+	dmat4 aMat = modelViewMat * mModelMat; 
+	upload(aMat); 
+	// Aquí se puede fijar el color de la esfera así: 
+	// glEnable(GL_COLOR_MATERIAL); 
+	// glColor3f(...); 
+	// Aquí se puede fijar el modo de dibujar la esfera: 
+	// gluQuadricDrawStyle(q, ...); 
+	// gluSphere(q, r, 50, 50); 
+	// Aquí se debe recuperar el color: 
+	glColor3f(1.0, 1.0, 1.0);
+}
