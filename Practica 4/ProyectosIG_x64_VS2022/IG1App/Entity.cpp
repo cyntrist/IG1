794c40b4c151080004ec56b796caaeee5d26ab6c
ineprimo <ineprimo@ucm.es>
2024-04-12 16:21:20 +0200

    boy if i

--- Practica 4/ProyectosIG_x64_VS2022/IG1App/Entity.cpp
+++ Practica 4/ProyectosIG_x64_VS2022/IG1App/Entity.cpp
@@ -684,3 +684,85 @@ void Sphere::render(glm::dmat4 const& modelViewMat) const
 	// Aquí se debe recuperar el color: 
 	glColor3f(1.0, 1.0, 1.0);
 }
+
+Cylinder::Cylinder(GLdouble brr, GLdouble trr, GLdouble hh)
+{
+	br = brr;
+	tr = trr;
+	h = hh;
+}
+
+void Cylinder::render(glm::dmat4 const& modelViewMat) const
+{
+
+	dmat4 aMat = modelViewMat * mModelMat;
+	upload(aMat);
+	// Aquí se puede fijar el color de la esfera así: 
+	glEnable(GL_COLOR_MATERIAL);
+	glColor3f(1.0, 0.0, 0.0);
+	// Aquí se puede fijar el modo de dibujar la esfera: 
+	gluQuadricDrawStyle(q, GLU_FILL);
+	// GLU_LINE
+	// GLU_SILHOUETTE
+	// GLU_POINT
+
+	
+	gluCylinder(q, br, tr, h, 50, 50);
+	// Aquí se debe recuperar el color: 
+	glColor3f(1.0, 1.0, 1.0);
+}
+
+
+
+Disk::Disk(GLdouble irr, GLdouble orr)
+{
+	iR = irr;
+	oR = orr;
+}
+
+void Disk::render(glm::dmat4 const& modelViewMat) const
+{
+
+	dmat4 aMat = modelViewMat * mModelMat;
+	upload(aMat);
+	// Aquí se puede fijar el color de la esfera así: 
+	glEnable(GL_COLOR_MATERIAL);
+	glColor3f(1.0, 0.0, 0.0);
+	// Aquí se puede fijar el modo de dibujar la esfera: 
+	gluQuadricDrawStyle(q, GLU_FILL);
+	// GLU_LINE
+	// GLU_SILHOUETTE
+	// GLU_POINT
+
+	gluDisk(q, iR, oR, 50, 50);
+	// Aquí se debe recuperar el color: 
+	glColor3f(1.0, 1.0, 1.0);
+}
+
+PartialDisk::PartialDisk(GLdouble irr, GLdouble orr, GLdouble sAng, GLdouble swAng)
+{
+	iR = irr;
+	oR = orr;
+	sa = sAng;
+	swa = swAng;
+
+}
+
+void PartialDisk::render(glm::dmat4 const& modelViewMat) const
+{
+	dmat4 aMat = modelViewMat * mModelMat;
+	upload(aMat);
+	// Aquí se puede fijar el color de la esfera así: 
+	glEnable(GL_COLOR_MATERIAL);
+	glColor3f(1.0, 0.0, 0.0);
+	// Aquí se puede fijar el modo de dibujar la esfera: 
+	gluQuadricDrawStyle(q, GLU_FILL);
+	// GLU_LINE
+	// GLU_SILHOUETTE
+	// GLU_POINT
+
+	gluPartialDisk(q, iR, oR, 50, 50, sa, swa);
+
+	// Aquí se debe recuperar el color: 
+	glColor3f(1.0, 1.0, 1.0);
+}
