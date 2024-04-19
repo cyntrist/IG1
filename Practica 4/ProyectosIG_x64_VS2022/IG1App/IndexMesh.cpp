1e8a595dc95b2d4dd3d35a795175b210505f6e29
cynthia <118978961+cyntrist@users.noreply.github.com>
2024-04-20 00:13:35 +0200

    voy a hacer el opcional

--- Practica 4/ProyectosIG_x64_VS2022/IG1App/IndexMesh.cpp
+++ Practica 4/ProyectosIG_x64_VS2022/IG1App/IndexMesh.cpp
@@ -72,22 +72,6 @@ IndexMesh* IndexMesh::generateIndexedBox(GLdouble l)
 	mesh->nNumIndices = 36;
 	mesh->vIndices = new GLuint[mesh->nNumIndices];
 
-	//GLuint auxIndices[] = {
-	//	0, 3, 2,
-	//	0, 2, 1,
-	//	4, 5, 6,
-	//	4, 6, 7,
-	//	0, 4, 7,
-	//	0, 7, 3,
-	//	1, 2, 6,
-	//	1, 6, 5,
-	//	0, 1, 5,
-	//	0, 5, 4,
-	//	4, 7, 6,
-	//	4, 3, 7
-	//};
-	//mesh->vIndices = auxIndices;
-
 	// cara de abajo
 	mesh->vIndices[0] = 0;
 	mesh->vIndices[1] = 3;
